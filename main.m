52ce450c965ad11dda6202f1237cb7f5de33876b
Mikołaj Bisewski <s188594@o365.student.pg.edu.pl>
2023-05-25 15:33:39 +0200

    added lagrange and splines

--- main.m
+++ main.m
@@ -1,1 +1,18 @@
 %Projekt3
+clear all;
+close all;
+
+%test
+A = importdata("./profile_wysokosciowe/Obiadek.csv");
+B = importdata("./profile_wysokosciowe/MountEverest.csv");
+C = importdata("./profile_wysokosciowe/WielkiKanionKolorado.csv");
+
+figure
+plot(A(:,1), A(:,2))
+
+figure
+plot(B.data(:,1), B.data(:,2))
+
+figure
+
+plot(C.data(:,1), C.data(:,2))
