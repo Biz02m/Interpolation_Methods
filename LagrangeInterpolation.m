d1bdacfd67a5ee92b46bd971b0af8286b8a69198
Mikołaj Bisewski <s188594@o365.student.pg.edu.pl>
2023-05-29 21:50:48 +0200

    fixed splines

--- LagrangeInterpolation.m
+++ LagrangeInterpolation.m
@@ -2,34 +2,40 @@
 % Metoda wielomianu lagrange!
 clear all;
 close all;
-A = importdata("./profile_wysokosciowe/GlebiaChallengera.csv");
-%A = importdata("./profile_wysokosciowe/MountEverest.csv");
-%A = importdata("./profile_wysokosciowe/WielkiKanionKolorado.csv");
-startOf = 1;
-endOf = size(A.data,1);
-numberOfInterpolationNodes = 11;
+Data(1) = importdata("./profile_wysokosciowe/GlebiaChallengera.csv");
+Titles(1) = "Głębia Challengera";
+Data(2) = importdata("./profile_wysokosciowe/SpacerniakGdansk.csv");
+Titles(2) = "Spacerniak w Gdańsku";
+Data(3) = importdata("./profile_wysokosciowe/WielkiKanionKolorado.csv");
+Titles(3) = "Wielki Kanion Kolorado";
 
-n = floor(endOf/(numberOfInterpolationNodes-1));
-if mod(n,2) == 0
-    n = n - 1;
+numberOfInterpolationNodes = 4;
+
+for i = 1:3
+    startOf = 1;
+    endOf = size(Data(i).data,1);
+    
+    n = floor(endOf/(numberOfInterpolationNodes-1));
+    if mod(n,2) == 0
+        n = n - 1;
+    end
+    
+    RealData = Data(i).data(startOf:endOf, :);
+    interpolationNodes = RealData(startOf:n:endOf, :);
+    
+    %figure
+    %plot(RealData(:,1),RealData(:,2), LineWidth=5, Color=[0.50 0.50 0.50]), xlabel("Dystans"), ylabel("Wysokość"), title(strcat("Profil wysokościowy - ", Titles(i)))
+    
+    interpolatedValues = lagrange(interpolationNodes, RealData);
+    
+    xmin = 0;
+    xmax = max(RealData(:,1));
+    ymin = min(RealData(:,2));
+    ymax = max(RealData(:,2)) + 10;
+    figure
+    plot(RealData(:,1),RealData(:,2), LineWidth=5, Color=[0.50 0.50 0.50]);
+    hold on
+    scatter(interpolationNodes(:,1),interpolationNodes(:,2), "cyan");
+    plot(interpolatedValues(:,1), interpolatedValues(:,2)), xlabel("Dystans"), ylabel("Wysokość"), title(strcat("Interpolacja wielomianem Lagrange - ",Titles(i))), xlim([xmin, xmax]), ylim([ymin, ymax])
+    saveas(cgf)
 end
-
-RealData = A.data(startOf:endOf, :);
-interpolationNodes = RealData(startOf:n:endOf, :);
-
-plot(RealData(:,1),RealData(:,2), LineWidth=5, Color=[0.50 0.50 0.50])
-hold on
-scatter(interpolationNodes(:,1),interpolationNodes(:,2), "cyan"), xlabel("Dystans"), ylabel("Wysokość"), title("Profil wysokościowy - Dane Rzeczywiste")
-
-interpolatedValues = lagrange(interpolationNodes, RealData);
-
-xmin = 0;
-xmax = max(RealData(:,1));
-ymin = min(RealData(:,2));
-ymax = max(RealData(:,2)) + 10;
-figure
-plot(RealData(:,1),RealData(:,2), LineWidth=5, Color=[0.50 0.50 0.50]);
-hold on
-scatter(interpolationNodes(:,1),interpolationNodes(:,2), "cyan");
-plot(interpolatedValues(:,1), interpolatedValues(:,2)), xlabel("Dystans"), ylabel("Wysokość"), title("Profil wysokościowy - Interpolacja Lagrange"), xlim([xmin, xmax]), ylim([ymin, ymax])
-
--- SplinesInterpolation.m
+++ SplinesInterpolation.m
@@ -2,52 +2,53 @@
 % Metoda funkcjami sklejanymi 3 stopnia
 clear all
 close all
-A = importdata("./profile_wysokosciowe/GlebiaChallengera.csv");
-%A = importdata("./profile_wysokosciowe/MountEverest.csv");
-%A = importdata("./profile_wysokosciowe/WielkiKanionKolorado.csv");
+Data(1) = importdata("./profile_wysokosciowe/GlebiaChallengera.csv");
+Titles(1) = "Głębia Challengera";
+Data(2) = importdata("./profile_wysokosciowe/SpacerniakGdansk.csv");
+Titles(2) = "Spacerniak w Gdańsku";
+Data(3) = importdata("./profile_wysokosciowe/WielkiKanionKolorado.csv");
+Titles(3) = "Wielki Kanion Kolorado";
 
-startOf = 1;
-endOf = size(A.data,1);
 numberOfInterpolationNodes = 25;
 
-n = floor(endOf/(numberOfInterpolationNodes-1));
-if mod(n,2) == 0
-    n = n - 1;
+for i = 1:3
+    startOf = 1;
+    endOf = size(Data(i).data,1);
+    
+    n = floor(endOf/(numberOfInterpolationNodes-1));
+    if mod(n,2) == 0
+        n = n - 1;
+    end
+    
+    RealData = Data(i).data(startOf:endOf, :);
+    interpolationNodes = RealData(startOf:n:endOf, :);
+    
+    %RealData = zeros(7, 2);  % Inicjalizacja macierzy 7x2 wypełnionej zerami
+    %for i = 1:7
+    %    RealData(i, 1) = i - 1;  % Wypełnienie pierwszej kolumny wartościami 0-6
+    %end
+    %RealData(2, 2) = 6;
+    %RealData(4,2) = -2
+    %RealData(6,2) = 4;
+    
+    %interpolationNodes = zeros(3,2);
+    %interpolationNodes(1,1) = 1;
+    %interpolationNodes(1,2) = 6;
+    %interpolationNodes(2,1) = 3;
+    %interpolationNodes(2,2) = -2;
+    %interpolationNodes(3,1) = 5;
+    %interpolationNodes(3,2) = 4;
+    
+    b = splines(interpolationNodes);
+    interpolatedValues = performSplineInterpolation(interpolationNodes,RealData,b);
+    
+    xmin = 0;
+    xmax = max(RealData(:,1));
+    ymin = min(RealData(:,2));
+    ymax = max(RealData(:,2)) + 10;
+    figure
+    plot(RealData(:,1),RealData(:,2), LineWidth=5, Color=[0.50 0.50 0.50]);
+    hold on
+    scatter(interpolationNodes(:,1),interpolationNodes(:,2), "cyan");
+    plot(interpolatedValues(:,1), interpolatedValues(:,2)), xlabel("Dystans"), ylabel("Wysokość"), title(strcat("Interpolacja splajnami 3 stopnia - ", Titles(i))), xlim([xmin, xmax]), ylim([ymin, ymax])
 end
-
-RealData = A.data(startOf:endOf, :);
-interpolationNodes = RealData(startOf:n:endOf, :);
-
-%RealData = zeros(7, 2);  % Inicjalizacja macierzy 7x2 wypełnionej zerami
-%for i = 1:7
-%    RealData(i, 1) = i - 1;  % Wypełnienie pierwszej kolumny wartościami 0-6
-%end
-%RealData(2, 2) = 6;
-%RealData(4,2) = -2
-%RealData(6,2) = 4;
-
-%interpolationNodes = zeros(3,2);
-%interpolationNodes(1,1) = 1;
-%interpolationNodes(1,2) = 6;
-%interpolationNodes(2,1) = 3;
-%interpolationNodes(2,2) = -2;
-%interpolationNodes(3,1) = 5;
-%interpolationNodes(3,2) = 4;
-
-
-plot(RealData(:,1),RealData(:,2), LineWidth=5, Color=[0.50 0.50 0.50])
-hold on
-scatter(interpolationNodes(:,1),interpolationNodes(:,2), "cyan"), xlabel("Dystans"), ylabel("Wysokość"), title("Profil wysokościowy - Dane Rzeczywiste")
-
-b = splines(interpolationNodes);
-interpolatedValues = performSplineInterpolation(interpolationNodes,RealData,b);
-
-xmin = 0;
-xmax = max(RealData(:,1));
-ymin = min(RealData(:,2));
-ymax = max(RealData(:,2)) + 10;
-figure
-plot(RealData(:,1),RealData(:,2), LineWidth=5, Color=[0.50 0.50 0.50]);
-hold on
-scatter(interpolationNodes(:,1),interpolationNodes(:,2), "cyan");
-plot(interpolatedValues(:,1), interpolatedValues(:,2)), xlabel("Dystans"), ylabel("Wysokość"), title("Profil wysokościowy - Interpolacja splajnami 3 stopnia"), xlim([xmin, xmax]), ylim([ymin, ymax])
